2321844bd3b6ebfbc9dffda0b900d4f2377ae754
Sinayel <yanis.louvel402@gmail.com>
2025-10-22 01:15:17 +0200

    Capuchino ?

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,22 +6,22 @@
 /*   By: yanis <yanis@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 17:09:50 by yanis             #+#    #+#             */
-/*   Updated: 2025/10/14 17:12:23 by yanis            ###   ########.fr       */
+/*   Updated: 2025/10/22 00:43:04 by yanis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#define PUSH_SWAP_H
 
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-typedef struct s_data
+typedef struct s_stack
 {
-	void	*data;
-	s_data	*next;
-}			t_data;
-
-
+	int nb;
+	int id;
+	struct s_stack *next;
+} t_stack;
 
 #endif
--- push_swap/test.c
+++ push_swap/test.c
@@ -1,85 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct s_data
 {
-	char			*name;
-	struct s_data	*next;
-}					t_data;
+    int i;
+    struct s_data *next;
+}               t_data;
 
-t_data	*get_data(void)
+t_data *init_lst(t_data *data, int nb)
 {
-	static t_data	data;
-
-	return (&data);
+    data = malloc(sizeof(t_data));
+    if(!data)
+        return NULL;
+    data->i = nb;
+    data->next = NULL;
+    return data;
 }
 
-// t_data	*init_lst(char *str)
-// {
-// 	t_data	*data;
-
-// 	data = malloc(sizeof(t_data));
-// 	if (!data)
-// 		return (NULL);
-// 	data->name = str;
-// 	data->next = NULL;
-// 	return (data);
-// }
-
-// t_data	*add_first(t_data *data, char *str)
-// {
-// 	t_data	*new_node;
-
-// 	new_node = malloc(sizeof(t_data));
-// 	if (!new_node)
-// 		return (data);
-// 	new_node->name = str;
-// 	new_node->next = data;
-// 	return (new_node);
-// }
-
-t_data *add_back(t_data *data, char *str)
+t_data *add_back(t_data *data, int str)
 {
     t_data *tmp;
     t_data *new_node;
     new_node = malloc(sizeof(t_data));
     if(!new_node)
-        return (data);
-    new_node->name = str;
+        return data;
+    new_node->i = str;
     new_node->next = NULL;
     if(!data)
-        return (new_node);
-    tmp = data;
-    while(tmp->next)
-        tmp = tmp->next;
-    tmp->next = new_node;
-
-    return (data);
-}
-
-t_data	*init_lst(char *str)
-{
-	t_data	*data;
-
-	data = malloc(sizeof(t_data));
-	if (!data)
-		return (NULL);
-	data->name = str;
-	data->next = NULL;
-	return (data);
-}
-
-t_data *add_back(t_data *data, char *str)
-{
-    t_data *tmp;
-    t_data *new_node;
-    new_node = malloc(sizeof(t_data));
-    if(!new_node)
-        return (data);
-    new_node->name = str;
-    new_node->next = NULL;
-    if(!data)
-        return (new_node);
+        return new_node;
     tmp = data;
     while(tmp->next)
         tmp = tmp->next;
@@ -87,31 +36,70 @@ t_data *add_back(t_data *data, char *str)
     return data;
 }
 
-t_data *add_front(t_data *data, char *str)
+t_data *add_front(t_data *data, int str)
 {
     t_data *new_node = malloc(sizeof(t_data));
     if(!new_node)
         return data;
-    new_node->name = str;
+    new_node->i = str;
     new_node->next = data;
     return new_node;
 }
 
-int	main(int argc, char *argv[])
+void print_list(t_data *data)
 {
-	if (argc >= 2)
-	{
-		t_data *data;
-		data = init_lst(argv[1]);
-		int i = 1;
-		while (i < argc - 1)
-			data = add_front(data, argv[++i]);
-		while (data)
-		{
-			printf("data->name = %s\ndata->ptr = %p\n", data->name, data);
-			if (data->next)
-				printf("\n");
-			data = data->next;
-		}
-	}
+    int i = 0;
+    while(data)
+    {
+        printf("%d\n", data->i);
+        data = data->next;
+        i++;
+    }
+}
+
+int	ascending(int a, int b)
+{
+	return (a <= b);
+}
+
+t_data	*sort_list(t_data *lst, int (*cmp)(int, int))
+{
+    t_data *tmp = lst;
+    int nb = 0;
+    if(tmp)
+    {
+        while(tmp && tmp->next)
+        {
+            if(!cmp(tmp->i, tmp->next->i))
+            {
+                nb = tmp->i;
+                tmp->i = tmp->next->i;
+                tmp->next->i = nb;
+                tmp = lst;
+            }
+            tmp = tmp->next;
+        }
+    }
+    return lst;
+}
+
+int main(int argc, char *argv[])
+{
+    t_data *data;
+    srand(time(NULL));
+    int rd = rand() % 100;
+    int i = 0;
+    data = init_lst(data, rd);
+    rd = rand() % 100;
+    while(++i < 10)
+    {
+        data = add_back(data, rd);
+        rd = rand() % 100;
+    }
+    printf("Liste avant trie :\n");
+    print_list(data);
+    printf("\nListe triee :\n");
+    data = sort_list(data, ascending);
+    print_list(data);
+    return 0;
 }
